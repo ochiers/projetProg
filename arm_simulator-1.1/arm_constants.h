ded796d6235f51337375c88a83e50d12a4a8e14a
ochiers <sebastien.ochier@gmail.com>
2015-01-06 14:50:52 +0100

    Definition des constantes Conditions et Instructions

--- arm_simulator-1.1/arm_constants.h
+++ arm_simulator-1.1/arm_constants.h
@@ -61,6 +61,52 @@ Contact: Guillaume.Huard@imag.fr
 #define PrivMask    0x0000000F
 #define StateMask   0x00000020
 
+
+/* Code Ops 
+	Intructions class - Position 27-26*/
+#define ARITHMETICS_OPERATION	0
+#define LOAD_STORE_OPERATION	1
+#define BRANCH_OPERATION		2
+
+/* Arithmetics Operations 
+	Position CodeOp 24-21 */
+#define INSTR_AND	0	//0000
+#define INSTR_EOR	1	//0001
+#define INSTR_SUB	2	//0010
+#define INSTR_RSB	3	//0011
+#define INSTR_ADD	4	//0100
+#define INSTR_ADC	5	//0101
+#define INSTR_SBC	6	//0110
+#define INSTR_RSC	7	//0111
+#define INSTR_TST	8	//1000
+#define INSTR_TEQ	9	//1001
+#define INSTR_CMP	10	//1010
+#define INSTR_CMN	11	//1011
+#define INSTR_ORR	12	//1100
+#define INSTR_MOV	13	//1101
+#define INSTR_BIC	14	//1110
+#define INSTR_MVN	15	//1111
+
+/* Arithmetics Operations 
+	Position CodeOp 24-21 */
+#define COND_EQ	0	//0000
+#define COND_NE	1	//0001
+#define COND_CS	2	//0010
+#define COND_CC	3	//0011
+#define COND_MI	4	//0100
+#define COND_PL	5	//0101
+#define COND_VS	6	//0110
+#define COND_VC	7	//0111
+#define COND_HI	8	//1000
+#define COND_LS	9	//1001
+#define COND_GE	10	//1010
+#define COND_LT	11	//1011
+#define COND_GT	12	//1100
+#define COND_LE	13	//1101
+#define COND_AL	14	//1110
+#define COND_-	15	//1111
+
+
 char *arm_get_exception_name(unsigned char exception);
 char *arm_get_mode_name(uint8_t mode);
 char *arm_get_register_name(uint8_t reg);
