c20223de64050b17f9645d4d1a5dbfc5f50939a0
simbadSid <sidlakhdar.riyane@gmail.com>
2015-01-08 09:30:10 +0100

    fin de data pross_processing: ca compile...

--- arm_simulator-1.1/util.c
+++ arm_simulator-1.1/util.c
@@ -36,9 +36,41 @@ uint32_t ror(uint32_t value, uint8_t rotation)
 {
 	return (value >> rotation) | (value << (32-rotation));
 }
-
 int is_big_endian()
 {
 	static uint32_t one = 1;
 	return ((* (uint8_t *) &one) == 0);
+}
+// ------------------------------------------
+// Calcule o1 OuExclusif o2
+// Parametre d'entree:
+//	- o1, o2:	Valeur des operandes 1 et 2
+// Parametre de sortie:
+//	- res:	resultat du calcul sur 32 bits
+// ------------------------------------------
+uint32_t eor(uint32_t o0, uint32_t o1)
+{
+	return (o0 ^ o1);
+}
+// ------------------------------------------
+// Calcule o1 Logical Shift Left ..
+// Parametre d'entree:
+//	- o1, o2:	Valeur des operandes 1 et 2 sur 64 bit (32 bit + des 0)
+// Parametre de sortie:
+//	- res:	resultat du calcul sur 32 bits
+// ------------------------------------------
+uint32_t lsl(uint32_t a, uint8_t shift)
+{
+	return (a << shift);
+}
+// ------------------------------------------
+// Calcule a Logical Shift Right ..
+// Parametre d'entree:
+//	- o1, o2:	Valeur des operandes 1 et 2 sur 64 bit (32 bit + des 0)
+// Parametre de sortie:
+//	- res:	resultat du calcul sur 32 bits
+// ------------------------------------------
+uint32_t lsr(uint32_t a, uint8_t shift)
+{
+	return (a >> shift);
 }
