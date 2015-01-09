7eb1b9328a5f2b40d6de3f8378dde89f093b42ee
MeeToo123 <louis.sellam@ujf-grenoble.fr>
2015-01-09 15:11:08 +0100

    Ajout de exemple_processing

--- arm_simulator-1.1/Examples/example_processing.s
+++ arm_simulator-1.1/Examples/example_processing.s
@@ -1,6 +1,16 @@
 .global main
 .text
 main:
+	mov r0, #0
+	mov r1, #1
+	mov r2, #2
+	mov r3, #3
+	mov r4, #4
+	mov r5, #5
+	mov r6, #6
+	mov r7, #7
+	mov r8, #8
+	
 	add	r0, #7 
 	add r0, r1
 	add r0, r0, #8
