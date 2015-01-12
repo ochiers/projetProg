b43afa1f66a2b312403f30443dadf57090bd56b6
ochiers <sebastien.ochier@gmail.com>
2015-01-12 10:07:40 +0100

    Ajout de test Load/Store

--- arm_simulator-1.1/Examples/exampleLoadStore.s
+++ arm_simulator-1.1/Examples/exampleLoadStore.s
@@ -1,36 +1,40 @@
 .global main
 .text
 main:
-    mov r0, #0x12
-    mov r7, #0x12
-    mov r9, #0x34
-    add r7, r9, r7, lsl #8
-    mov r9, #0x56
-    add r7, r9, r7, lsl #8
-    mov r9, #0x78
-    add r7, r9, r7, lsl #8
-    mov r9, #0x55
-    mov r12, #0x87
-    add r9, r12, r9, lsl #8
-    mov r6, #0x80
-    mov r10, #0x81
-    str r7, [r6]
-    mov r6, #0x80
-    strb r9, [r10]
-    ldrb r8, [r6]
-    mov r1, #0x34
-    add r0, r1, r0, lsl #8
-    mov r1, #0x56
-    add r0, r1, r0, lsl #8
-    mov r1, #0x78
-    add r0, r1, r0, lsl #8
-    mov r1, #0x2000
-    str r0, [r1]
-    ldrb r2, [r1]
-    add r1, r1, #3
-    ldrb r3, [r1]
-    swi 0x123456
+    bl init
 
+fin:
+    swi 0x123456
+    
+init:
+	mov r0, #0x12
+	mov r1, #0x34
+	add r0, r1, r0, lsl #8
+	mov r1, #0x56
+	add r0, r1, r0, lsl #8
+	mov r1, #0x78
+	add r0, r1, r0, lsl #8
+	mov r1, #0x87
+	mov r2, #0x65
+	add r1, r2, r1, lsl #8
+	mov r2, #0x43
+	mov r3, #0x21
+	mov r4, #0xFF
+	
+testldr:
+	str r3, [r4]
+	ldr r5, [r4]
+	
+testldrb:
+	strb r0, [r4]
+	ldrb r6, [r4]
+	
+testldrh:
+	strh r1, [r4]
+	ldrh r7, [r4]
+	bl fin
+	
+	
 adr_valeur: .word valeur
 
 .data
